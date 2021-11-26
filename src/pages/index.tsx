3ef1790aa4b01411f35a02f73152ed6d68d8440a
mancinilucas <mancini.lucasg@gmail.com>
2021-11-25 22:15:38 -0300

    global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
+import Head from 'next/head';
+//permite importar o head do "_doc" e acrescentar conteúdo na tag em questão em qualquer local do código
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+    <Head>
+      <title>Início | ig.news</title>
+    </Head>
+    <h1>
+    Hello World
+    </h1>
+    </>
+  )
 }
+//O conteúdo do 'title' foi adicionado dentro do Head do component '_doc'
