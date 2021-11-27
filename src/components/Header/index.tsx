9a3e8e4c5ad373b21457808e39d3eb4c7ffed675
mancinilucas <mancini.lucasg@gmail.com>
2021-11-26 22:24:04 -0300

    header basico

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
+import styles from './styles.module.scss';
+
 export function Header(){
   return(
-    <header>
-      <div>
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news"/>
         <nav>
-          <a>Home</a>
+          <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
       </div>
