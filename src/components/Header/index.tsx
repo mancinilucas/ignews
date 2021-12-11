8ab92789ef4782cc7316cc70dfad4a8765a396e0
mancinilucas <mancini.lucasg@gmail.com>
2021-12-11 19:48:26 -0300

    posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,8 @@
+import Link from 'next/link'
+import { ActiveLink } from '../ActiveLink';
+
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header(){
@@ -7,8 +11,12 @@ export function Header(){
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news"/>
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a >Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts"> 
+            <a>Posts</a>
+          </ActiveLink>          
         </nav>
 
         <SignInButton/>
